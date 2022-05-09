03fb2734f71f4725d1aec3857926ece069bce233
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-05-09 22:39:21 +0600

    feat: make stuff

---
+++ .vuepress/config.ts
@@ -0,0 +1,47 @@
+import { ThemeConfig } from "vuepress-theme-vt";
+import { defineConfig4CustomTheme } from "vuepress/config";
+
+export default defineConfig4CustomTheme<ThemeConfig>((ctx) => ({
+    theme: "vt",
+    title: "Xonia",
+    themeConfig: {
+        enableDarkMode: true,
+        repo: "https://github.com/xoniaapp/docs",
+        logo: "/logo.svg",
+        nav: [
+            {
+                text: "Website", link: "https://app.xoniaapp.com/"
+            },
+        ],
+        sidebar: {
+            "/docs/": [
+                {
+                    title: "Overview",
+                    collapsable: false,
+                    children: [
+                        "/docs/",
+                        "/docs/getting-started"
+                    ],
+                },
+                {
+                    title: "Contact",
+                    collapsable: false,
+                    children: [
+                        ['https://xoniaapp.com/support', 'Helpdesk'],
+                    ],
+                },
+            ],
+        },
+        codeSwitcher: {
+            groups: {
+                default: {
+                    ts: 'TypeScript',
+                    js: 'JavaScript',
+                    go: 'Golang',
+                    rs: 'Rust',
+                },
+                'plugin-usage': { tuple: 'Tuple', object: 'Object' },
+            }
+        }
+    },
+}));
---
+++ index.md
@@ -0,0 +1,19 @@
+---
+home: true
+heroImage: /logo.svg
+actionText: Getting Started
+actionLink: /docs/
+subActionText: Quick Start
+subActionLink: /docs/getting-started.html
+
+---
+
+::: slot heroText
+<b class="gradient">Xonia </b>Documentation
+:::
+
+::: slot footer
+
+<br>
+Copyright © 2022 Xonia.
+:::
